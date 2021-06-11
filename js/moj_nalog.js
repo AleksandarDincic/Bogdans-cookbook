4627aac088c787ca77f58680024a310b4cf76272
Aleksandar Dincic <a.dincic99@gmail.com>
2021-06-11 17:14:14 +0200

    boutta break

--- js/moj_nalog.js
+++ js/moj_nalog.js
@@ -35,6 +35,7 @@ function getTop3Recipes() {
 
     for (let i = 0; i < recipes.length && i < 3; ++i) {
         appendRecipeCard(recipes[i]);
+        appendRatingCard(recipes[i]);
     }
 }
 
@@ -69,14 +70,13 @@ function appendRecipeCard(recipe) {
     cardBody.append(recipeLeft);
 
     let recipeName = $("<h1></h1>").addClass("card-title").html(recipe.name);
-    let author = getUserName(recipe.author);
-    let recipeAuthor = $("<h5></h5>").addClass("card-text").html(author.name);
     let type = getRecipeType(recipe.type);
     let recipeType = $("<p></p>").addClass("card-text").addClass("text-muted").html(type.nameSRB);
-
+    let removeButton = $("<button></button>").addClass("btn").addClass("btn-danger").html("Ukloni");
+        
     recipeLeft.append(recipeName);
-    recipeLeft.append(recipeAuthor);
     recipeLeft.append(recipeType);
+    recipeLeft.append(removeButton);
 
     let recipeRight = $("<div></div>").addClass("recipe-right");
     cardBody.append(recipeRight);
@@ -101,4 +101,61 @@ function appendRecipeCard(recipe) {
     recipeRight.append(difficulty);
 
     $("#recipes-col").append(card);
+}
+
+function appendRatingCard(recipe) {
+    let card = $("<div></div>").addClass("card").addClass("mb-3").addClass("mt-3");
+    
+    let innerRow = $("<div></div>").addClass("row").addClass("no-gutters");
+    card.append(innerRow);
+    
+    let imgCol = $("<div></div>").addClass("col-md-4");
+    innerRow.append(imgCol);
+    
+    let img = $("<img></img>").addClass("card-img").addClass("recipe-img").attr("src", "img/chicken.jpg");
+    imgCol.append(img);
+
+    let bodyCol = $("<div></div>").addClass("col-md-8");
+    innerRow.append(bodyCol);
+
+    let cardBody = $("<div></div>").addClass("card-body");
+    bodyCol.append(cardBody);
+
+    let recipeLeft = $("<div></div>").addClass("recipe-left");
+    cardBody.append(recipeLeft);
+
+    let recipeName = $("<h1></h1>").addClass("card-title").html(recipe.name);
+    let myRating = $("<h5></h5>").addClass("card-text").html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i>");
+    let type = getRecipeType(recipe.type);
+    let recipeType = $("<p></p>").addClass("card-text").addClass("text-muted").html(type.nameSRB);
+    
+        
+    recipeLeft.append(recipeName);
+    recipeLeft.append(myRating);
+    recipeLeft.append(recipeType);
+    
+
+    let recipeRight = $("<div></div>").addClass("recipe-right");
+    cardBody.append(recipeRight);
+
+    let rating = $("<p></p>").addClass("card-text");
+    let ratingIcon = $("<i></i>").addClass("fas").addClass("fa-star");
+    let avg = getAvg(recipe);
+    rating.append(avg.toFixed(1) + "/5.0" + "&nbsp;&nbsp;");
+    rating.append(ratingIcon);
+    recipeRight.append(rating);
+
+    let time = $("<p></p>").addClass("card-text");
+    let timeIcon = $("<i></i>").addClass("fas").addClass("fa-clock");
+    time.append(recipe.time + "&nbsp;&nbsp;");
+    time.append(timeIcon);
+    recipeRight.append(time);
+
+    let difficulty = $("<p></p>").addClass("card-text");
+    let diffIcon = $("<i></i>").addClass("fas").addClass("fa-book-dead");
+    difficulty.append(recipe.difficulty + "/5" + "&nbsp;&nbsp;");
+    difficulty.append(diffIcon);
+    recipeRight.append(difficulty);
+
+    $("#ratings-col").append(card);
 }
